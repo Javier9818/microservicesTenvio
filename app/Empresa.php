c6e371a2f143c3efa8c56a49f9146145370bedae
Javier9818 <rodolfo180498@hotmail.com>
2020-08-29 23:04:13 -0500

    Buscar productos por token, id o  nombre unico

--- app/Empresa.php
+++ app/Empresa.php
@@ -38,7 +38,7 @@ class Empresa extends Model
       ->join('categorias_menus', 'productos.categorias_menu_id', '=', 'categorias_menus.id')
       ->join('empresas', 'empresas.id', '=', 'productos.empresa_id')
       ->selectRaw('productos.*, categorias_menus.descripcion as categoria')
-      ->whereRaw('empresas.token_fb = ? or empresas.id = ?, or empresas.nombre_unico = ?', [$token, $token, $token])
+      ->whereRaw('empresas.token_fb = ? or empresas.id = ? or empresas.nombre_unico = ?', [$token, $token, $token])
       ->get();
 
       return $productos;
